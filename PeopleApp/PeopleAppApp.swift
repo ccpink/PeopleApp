980c07d686ddfe2c896abed37a69f28e4b1258dd
Charles Pink <ccpink99@gmail.com>
2022-07-24 23:20:51 -0300

    Initial Commit

---
+++ PeopleApp/PeopleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleAppApp.swift
+//  PeopleApp
+//
+//  Created by Charles Pink on 2022-07-24.
+//
+
+import SwiftUI
+
+@main
+struct PeopleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
